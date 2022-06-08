a7208d6a7571a8c513e7a4481e3373a5985b7294
ChristianDaniel <christian_daniel04@hotmail.com>
2022-06-07 21:52:35 -0300

    create header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Header } from "./components/Header";
 import "./global.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return( 
+    <>
+      <Header />
+      <h1>Hello world</h1>;
+    </>
+  )
 }
 
 export default App;
